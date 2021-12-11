a489e75572f6fc19dc8bcad1cf5b719d7b497d81
matsaugustson <78408726+matsaugustson@users.noreply.github.com>
2021-12-11 23:38:28 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
-radio.onReceivedString(function (receivedString) {
+radio.onReceivedNumber(function (receivedNumber) {
     seconds = 0
-    basic.showString(receivedString)
+    basic.showNumber(receivedNumber)
 })
 let seconds = 0
 radio.setGroup(1)
 seconds = 0
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     pins.servoWritePin(AnalogPin.P0, input.lightLevel() / 256 * 180)
 })
 basic.forever(function () {
     basic.pause(1000)
     seconds += 1
-    if (seconds > 10) {
+    if (seconds > 60) {
         soundExpression.giggle.playUntilDone()
+        basic.showIcon(IconNames.Sad)
         seconds = 0
     }
 })
